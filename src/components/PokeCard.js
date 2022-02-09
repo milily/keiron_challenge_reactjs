f4b7b8716c2903c3d0766426dc800d9c40269010
mrise <mrise@mrise.local>
2022-02-08 21:02:46 -0300

    add loading an chips for pokemon types

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -2,14 +2,17 @@ import React, { Fragment, useEffect, useState} from "react";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Chip from '@mui/material/Chip';
 import { CardActionArea } from '@mui/material';
 import PokeSprite from "./PokeSprite";
+import pokeball from './../assets/pokeball.png'
 import axios from "axios";
+import '../App.css'
 
 const PokeCard = ({pokename}) => {
 
     const [pokeInfo, setPokeInfo] = useState([])
-
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(()=>{
         const singlePokeInfo = () => {
@@ -17,6 +20,7 @@ const PokeCard = ({pokename}) => {
                 .then((response) => {
                     const apiresponse = response.data;
                     setPokeInfo(apiresponse);
+                    setIsLoading(false)
                     console.log("pokecard",apiresponse)
                 }
             )
@@ -24,6 +28,16 @@ const PokeCard = ({pokename}) => {
         singlePokeInfo()
     },[])
 
+    if(isLoading){
+        return(
+            <img
+                width={'70'}
+                src={pokeball}
+                alt={'pokemon logo'}
+                className="pokeball"
+            />
+        )
+    }
 
     return(
         <Fragment>
@@ -36,7 +50,7 @@ const PokeCard = ({pokename}) => {
                             sx={{ fontSize: 14, fontFamily: 'Monospace' }} 
                             color="text.secondary" 
                             component="div">
-                            
+                            #{pokeInfo.id.toString().padStart(3, '0')}
                         </Typography>
                         <Typography 
                             gutterBottom 
@@ -50,7 +64,18 @@ const PokeCard = ({pokename}) => {
                             component={'span'} 
                             variant="body2" 
                             color="text.secondary">
-                            
+                            {pokeInfo.types.map((pokeType,index) => {
+                                return (
+                                    <Chip 
+                                        key={index} 
+                                        style={{marginRight: 5}} 
+                                        label={pokeType.type.name} 
+                                        size="small" 
+                                        color="primary" 
+                                        variant="outlined" />
+                                    )
+                                })
+                            }
                         </Typography>
                     </CardContent>
                 </CardActionArea>
